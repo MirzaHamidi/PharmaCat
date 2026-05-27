f91149d594678043ffdd86b8f6378b1547a47e2d
Mirza Hamidi <mmirzahamidi@gmail.com>
2026-05-27 19:28:00 +0300

    added nothing

--- obj/Debug/net9.0/PharmaCat.AssemblyInfo.cs
+++ obj/Debug/net9.0/PharmaCat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaCat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70ca084fb156cc5068758e9ffffd65587a3d37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40df6048ba1a204c0ba8cd93d34668975b565a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaCat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaCat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
